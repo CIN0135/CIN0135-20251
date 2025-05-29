c1f74da3e0fe22aa499ec261941420c5f57b7e13
paguso <paguso@cin.ufpe.br>
2025-05-29 17:00:32 -0300

    list operations

--- aulas/aula08/src/list.cpp
+++ aulas/aula08/src/list.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <cassert>
+
 struct Node {
 	int val;
 	Node* next;
@@ -23,9 +26,86 @@ void listAppend(Node *head, int v) {
 	cur->next = nn;
 }
 
+
+Node* listNodeAt(Node *head, size_t pos) {
+	Node *cur = head;
+	size_t i = 0;
+	while( cur->next != nullptr && i < pos ) {
+		cur = cur->next;
+		i++;
+	}
+	return cur;
+} 
+
+
+Node* listFind(Node *head, int value) {
+	Node *cur = head;
+	while( cur->next != nullptr && cur->next->val != value ) {
+		cur = cur->next;
+	}
+	return cur;
+} 
+
+
+void listInsert(Node *cur, int value) {
+	assert(cur!=nullptr);
+	Node *nn = newNode(value);
+	nn->next = cur->next;
+	cur->next = nn;
+}
+
+
+void listInsertAt(Node *head, size_t pos, int value) {
+	listInsert(listNodeAt(head, pos), value);
+}
+
+
+void listDelete(Node* cur) {
+	assert(cur->next != nullptr);
+	Node *toDie = cur->next;
+	cur->next = toDie->next;
+	delete toDie;
+}
+
+void listDeleteAt(Node *head, size_t pos) {
+	listDelete(listNodeAt(head, pos));
+}
+
+
 int main() {
 	Node *head = initEmptyList(); 
-	for (int i = 1; i < 5; i++) {
-		listAppend(head, i);
+	for (int val = 10; val < 100; val+=10) {
+		listAppend(head, val);
 	}	
+
+	for (size_t pos = 0; pos < 7; pos++) {
+		Node* node = listNodeAt(head, pos);
+		if (node->next!=nullptr) {
+			std::cout << "list[" << pos << "] = " << node->next->val << std::endl; 
+		} else {
+			std::cerr << "Posicao [" << pos << "] invalida" << std::endl;
+			break;
+		}
+	}
+
+	for (int value = 0; value < 100; value += 6 ) {
+		Node *node = listFind(head, value);
+		if (node->next != nullptr) {
+			std::cout << "value " << value << " encontrado" << std::endl; 
+		} else {
+			std::cout << "value " << value << " NAO encontrado" << std::endl; 
+		}
+	}
+
+	for (int value = 5, i = 0; value < 100; value += 10, i+=2) {
+		listInsertAt(head, i, value); 
+	}
+
+	
+	for (size_t i = 0; i < 12; i +=3) {
+		listDeleteAt(head, i); 
+	}
+
+
+
 }
